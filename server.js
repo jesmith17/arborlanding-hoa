9d8822f0742ae8a33cea8bde0d4952f5804f082f
Josh Smith <jesmith17@gmail.com>
2018-11-07 22:05:17 -0600

    Attempt 24

--- server.js
+++ server.js
@@ -4,13 +4,26 @@ const path = require('path');
 
 const app = express();
 
+const env = process.env.NODE_ENV || 'development';
+
+let forceSSL = (req, res, next) => {
+  if (req.headers['x-forwarded-proto'] !== 'https') {
+    return res.redirect(['https://', req.get('Host'), req.url].join(''));
+  }
+  return next();
+};
+
+if (env === 'production') {
+  app.use(forceSSL);
+}
+
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/'));
+app.use(express.static(__dirname + '/dist/arborhoa/'));
 
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/arborhoa/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
