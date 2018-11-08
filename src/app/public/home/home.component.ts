a65fc0e6bccf619cfdd854c3edf8714c570471d7
Josh Smith <jesmith17@gmail.com>
2018-11-07 21:46:41 -0600

    Fixed error preventing the prod build

--- src/app/public/home/home.component.ts
+++ src/app/public/home/home.component.ts
@@ -1,10 +1,6 @@
-import { Component, ElementRef, Inject, OnInit, Renderer, ViewChild} from '@angular/core';
+import { Component,  OnInit, ViewChild} from '@angular/core';
 import {NavbarComponent} from '../navbar/navbar.component';
 import {Subscription} from 'rxjs';
-import { DOCUMENT } from '@angular/platform-browser';
-import {NavigationEnd, Router} from '@angular/router';
-import { LocationStrategy, PlatformLocation, Location } from '@angular/common';
-import {filter} from 'rxjs/internal/operators';
 
 @Component({
   selector: 'app-home',
